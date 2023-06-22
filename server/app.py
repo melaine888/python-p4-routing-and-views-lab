a0755966f3e63ec1dd5cdc98c7825e0510b410a0
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-06-22 08:16:29 +0300

    completed routing

--- server/app.py
+++ server/app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string_param>')
+def print_string(string_param):
+    print(string_param)  
+    return string_param
+
+
+@app.route('/count/<int:int_param>')
+def count(int_param):
+    numbers = "\n".join(str(num) for num in range(int_param + 1))
+    return numbers
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    return str(result)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
